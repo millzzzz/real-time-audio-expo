ffdf45d82536e8278dc939ffb7e25ea5c891b31f
millzzzz <senik456@seznam.cz>
2025-02-04 23:58:19 -0500

    fresh Expo project :fire:

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // Add custom rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
